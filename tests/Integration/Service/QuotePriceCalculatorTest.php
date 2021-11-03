a70b003e55fbae62df1e5a0ace39aef4291f7036
Giacomo Lombardi <giacomo.lombardi@slope.it>
2021-11-03 11:44:00 +0100

    Issue #1465: fix another test

--- tests/Integration/Service/QuotePriceCalculatorTest.php
+++ tests/Integration/Service/QuotePriceCalculatorTest.php
@@ -25,7 +25,7 @@ class QuotePriceCalculatorTest extends KernelTestCase
         // Action
         $price = $SUT->calculate($arrivalDate, $nightsCount, $guestCount);
 
-        // Expectations: 80€ * 2 nights * 1 (no discount)
-        $this->assertEquals(80 * 2, $price);
+        // Expectations: 80€ * 2 nights * 3 guests * 1 (no discount)
+        $this->assertEquals(80 * 2 * 3, $price);
     }
 }
